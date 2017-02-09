2a4e76a9d747c2d1ec1882d6fded9dd032c975f8
soloproyectos <gonzalo.chumillas@gmail.com>
2017-02-09 18:50:21 +0100

    update example

--- example/modal_dialog_example.dart
+++ example/modal_dialog_example.dart
@@ -1,1 +1,9 @@
-void main() {}
+//import 'package:modal_dialog/core.dart';
+import 'package:modal_dialog/modal_dialog.dart';
+
+main() async {
+  await alert('It works!');
+  if (await confirm('Are you sure?')) {
+    print('ok');
+  }
+}
