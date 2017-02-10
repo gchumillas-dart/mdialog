94d2f1cbb2f95b6b5d2165b236d480e98eb6a42d
soloproyectos <gonzalo.chumillas@gmail.com>
2017-02-10 14:32:58 +0100

    update test

--- example/test.dart
+++ example/test.dart
@@ -3,8 +3,10 @@ import 'dart:async';
 import 'package:modal_dialog/modal_dialog.dart';
 
 Future<Null> main() async {
-  await alert('It works!');
+  await alert('We will proceed to format the Hard Disk');
+  print('Ok, you\'ve been alerted');
+
   if (await confirm('Are you sure?')) {
-    print('ok');
+    print('Formating Hard Disk ...');
   }
 }
